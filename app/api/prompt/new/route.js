2bfbeef9f54c9db8fc4034948688f35cc6d197b1
Udeh Tochukwu <redefineddevelopertee@gmail.com>
2024-01-05 23:59:26 +0100

    created the form component and attended to the backend endpoints

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,13 @@
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req) => {
+    const {userId, prompt, tag} = await req .json();
+
+
+    try {
+        await connectToDB();
+        
+    } catch (error) {
+        
+    }
+}
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+'use client'
+import {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import Form from '@component/Form'
 
 const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const[post, setPost] =useState({
+    prompt: '',
+    tag: ''
+  });
+
+  const createPrompt =async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if(response.ok){
+        Router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally{
+      setSubmitting(false)
+    }
+  }
   return (
-    <div>This is no longer empty</div>
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+
   )
 }
 
